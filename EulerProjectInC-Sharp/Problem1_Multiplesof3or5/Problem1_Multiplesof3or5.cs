b51a7867040a09c0b7b94fdce0c948c9482d2b8c
Loïc Miest <155390470+MiestLo@users.noreply.github.com>
2024-01-11 18:05:44 +0100

    Euler project Problem #1 - Multiples of 3 or 5

---
+++ EulerProjectInC-Sharp/Problem1_Multiplesof3or5/Problem1_Multiplesof3or5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProjectInC_Sharp
+{ 
+    public class Problem1_Multiplesof3or5
+    {
+        public void Problem1()
+        {
+          int result = 0;
+          for (int a = 0; a < 1000; a++)
+            {
+                if (3 * a < 1000)
+                    result += 3 * a;
+                if (5 * a < 1000)
+                    result += 5 * a;
+            }
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
--- EulerProjectInC-Sharp/Program.cs
+++ EulerProjectInC-Sharp/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Problem
 
 namespace EulerProjectInC_Sharp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static Problem1_Multiplesof3or5 AccesProblem1 = new Problem1_Multiplesof3or5();
+        static void Main() //static void Main(string[] args)
         {
+            AccesProblem1.Problem1();
         }
     }
 }
